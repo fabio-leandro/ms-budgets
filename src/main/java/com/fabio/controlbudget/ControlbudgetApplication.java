1c5dda23917d79a4ea52c5d5dc817002ee39b40f
Fabio Leandro <fabiolpsolucoes@gmail.com>
2021-10-27 09:47:44 -0300

    Iniciando com SPRING BOOT INITIALIZR

---
+++ src/main/java/com/fabio/controlbudget/ControlbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.controlbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio/controlbudget/ControlbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.controlbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
